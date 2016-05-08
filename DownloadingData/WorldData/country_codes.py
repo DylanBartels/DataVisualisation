f02edf8511d0390df2b5f897e264bf1c487b6011
Dylan Bartels <d_bartels@live.nl>
2016-05-08 18:22:08 +0200

    Initial commit

---
+++ DownloadingData/WorldData/country_codes.py
@@ -0,0 +1,30 @@
+from pygal_maps_world.i18n import COUNTRIES
+
+def get_country_code(country_name):
+    """Return the Pygal 2-digit country code for the given country"""
+
+    for code, name in COUNTRIES.items():
+        if name == country_name:
+            return code
+        elif country_name == 'Yemen, Rep.':
+            return 'ye'
+        elif country_name == 'Vietnam':
+            return 'vn'
+        elif country_name == 'Egypt, Arab Rep.':
+            return 'eg'
+        elif country_name == 'Bolivia':
+            return 'bo'
+        elif country_name == 'Libya':
+            return 'ly'
+        elif country_name == 'Congo, Dem. Rep.':
+            return 'cd'
+        elif country_name == 'Congo, Rep.':
+            return 'cg'
+        elif country_name == 'Iran, Islamic Rep.':
+            return 'ir'
+        elif country_name == 'Venezuela, RB':
+            return 've'
+        elif country_name == 'Tanzania':
+            return 'tz'
+    # If the country wasn't found, return None.
+    return None
